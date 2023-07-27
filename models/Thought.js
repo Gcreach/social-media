6e669bf8471e45f577c2178fd54f2f7b12881e93
Gcreach <gcreach97@gmail.com>
2023-07-27 12:17:21 -0700

    updated models and others

---
+++ models/Thought.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        //character limit here
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      username: {
+        type: String,
+        required: true,
+      },
+      reactions: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Reaction',
+        },
+    ],
+    }
+);
+
+thoughtSchema 
+    .virtual('reactionCount')
+    .get(function () {
+      return `${this.reactions}`;
+    });
+
+
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -21,10 +21,14 @@ const userSchema = new Schema(
         ref: 'Thought',
       },
     ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Friend',
+        },
+    ],
   },
   {
-    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
     toJSON: {
       virtuals: true,
     },
@@ -32,3 +36,12 @@ const userSchema = new Schema(
   }
 );
 
+userSchema
+  .virtual('friendCount')
+  .get(function () {
+    return `${this.friends}`;
+  });
+
+  const User = model('user', userSchema);
+
+  module.exports = User;
